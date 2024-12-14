038d5814a2ce278d8a9db7c4c7d5c14dac21cd20
vaibhav <vaibhavc121@gmail.com>
2024-12-14 22:45:45 +0530

    14-12

---
+++ Enfinity_Automation/TestCases/HRMS/HRCore/Json_DesignationTest.cs
@@ -0,0 +1,51 @@
+﻿using Enfinity_Automation.BaseTest;
+using Enfinity_Automation.PageObjects.HRMS.HRCore;
+using Enfinity_Automation.Utilities.DataProviders.HRMS.HRCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enfinity_Automation.TestCases.HRMS.HRCore
+{
+    public class Json_DesignationTest : BaseClass
+    {
+        private DesignationPage _dp;
+
+        [SetUp]
+        public void Init()
+        {
+            // Initialize the DesignationPage object with the driver
+            _dp = new DesignationPage(Driver);
+        }
+
+        [Test]
+        [Category("ddt")]       
+        public void VerifyDesignation()
+        {
+
+            // Navigate to Designation setup
+            _dp.NavigateToDesignation();
+
+            //_dp.CreateDesignation();
+
+            _dp.ClkNewBtn();
+            Logger.Info("clicked on new btn");
+            //_dp.EnterCode(code);
+            _dp.EnterCode(faker.Random.AlphaNumeric(5));
+            Logger.Info("provided code");
+            //_dp.EnterDesignation(desg);
+            _dp.EnterDesignation(faker.Name.JobTitle());
+            Logger.Info("provided designation");
+            _dp.ClkSaveBtn();
+            Logger.Info("clicked on save button");
+
+
+
+            Assert.True(_dp.isDesgCreated());
+
+            //test
+        }
+    }
+}
---
+++ Enfinity_Automation/TestCases/HRMS/HRCore/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enfinity_Automation.TestCases.HRMS.HRCore
+{
+    internal class Test
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("vaibhav");
+        }
+    }
+}
